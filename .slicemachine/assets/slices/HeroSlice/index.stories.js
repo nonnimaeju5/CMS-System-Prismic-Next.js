cfeffe334c753c57d16fd09bfdcf54a09e152614
Jón Ólafur <jolafur4@gmail.com>
2023-02-21 14:48:16 +0000

    on  going wing site

---
+++ .slicemachine/assets/slices/HeroSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/HeroSlice';
+
+export default {
+  title: 'slices/HeroSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"cta_link":{"link_type":"Web","url":"https://prismic.io"},"cta_text":"everyone"}],"primary":{"title":[{"type":"heading1","text":"Figure","spans":[]}],"description":[{"type":"paragraph","text":"Mollit sint nostrud ipsum proident.","spans":[]}],"image":{"dimensions":{"width":4380,"height":2895},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1537498425277-c283d32ef9db"}},"id":"_Default","slice_type":"hero_slice"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/ImageGrid/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ImageGrid';
+
+export default {
+  title: 'slices/ImageGrid'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"image":{"dimensions":{"width":4000,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1591012911207-0dbac31f37da"}}],"primary":{"title":[{"type":"heading1","text":"Widely","spans":[]}],"description":[{"type":"paragraph","text":"Proident laborum cillum id id incididunt et magna id do proident excepteur esse dolore dolore commodo.","spans":[]}]},"id":"_Default","slice_type":"image_grid"}} />
+_Default.storyName = ''
