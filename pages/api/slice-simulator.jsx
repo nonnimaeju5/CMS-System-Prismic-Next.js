4ad8496e0bc650f06e4da52c3e22b74a258d3b61
Jón Ólafur <jolafur4@gmail.com>
2023-02-22 10:04:55 +0000

    solving errors for deployment

--- pages/api/slice-simulator.jsx
+++ pages/api/slice-simulator.jsx
@@ -1,22 +1,20 @@
 import { SliceSimulator } from '@prismicio/slice-simulator-react'
 import { SliceZone } from '@prismicio/react'
- 
-import { components } from '../../slices'
-import state from '../../.slicemachine/libraries-state.json'
- 
+
+import { components } from '../slices'
+import state from '../.slicemachine/libraries-state.json'
+
 const SliceSimulatorPage = () => {
   return (
     <SliceSimulator
-      sliceZone={({ slices }) => (
-        <SliceZone slices={slices} components={components} />
-      )}
+      sliceZone={({ slices }) => <SliceZone slices={slices} components={components} />}
       state={state}
     />
   )
 }
- 
+
 export default SliceSimulatorPage
- 
+
 // Only include this page in development
 export const getStaticProps = async () => {
   if (process.env.NODE_ENV === 'production') {
